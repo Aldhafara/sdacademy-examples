c77e8c1d2b63408b804ffa09be43a35b8f6daf71
Aldhafara <aldhafara@onet.pl>
2018-06-14 14:25:46 +0200

    Add FizzBuzzWhiz solution

--- 26-tdd/src/main/java/pl/sdacademy/tdd/FizzBuzz.java
+++ 26-tdd/src/main/java/pl/sdacademy/tdd/FizzBuzz.java
@@ -1,17 +1,44 @@
 package pl.sdacademy.tdd;
 
-class FizzBuzz {
-	/**
-	 * Returns a "Fizz" when {@code number} if divisible by 3, "Buzz" when divisible by 5, "FizzBuzz" when
-	 * divisible by 3 and 5, and finally a string representation of a {@code number} in any other case (e.g. "1"
-	 * for 1, "2" for 2).
-	 *
-	 * @param number a number
-	 * @return the string according to the specification
-	 */
-	static String msg(int number) {
-		// TODO: implement according to javadoc specification following TDD principles
-		throw new UnsupportedOperationException("TODO: implement according to javadoc specification following"
-			+ " TDD principles");
-	}
+public class FizzBuzz {
+		/**
+		 * Returns a "Fizz" when {@code number} if divisible by 3, "Buzz" when divisible by 5, "FizzBuzz" when
+		 * divisible by 3 and 5, and finally a string representation of a {@code number} in any other case (e.g. "1"
+		 * for 1, "2" for 2).
+		 *
+		 * @param number a number
+		 * @return the string according to the specification
+		 */
+		public static String msg(int number) {
+			// TODO: implement according to javadoc specification following TDD principles  hgjgjhg
+			if (isPrime(number)==true){
+				if (number == 3)
+					return "FizzWhiz";
+				if (number == 5)
+					return "BuzzWhiz";
+				else
+					return "Whiz";
+			}
+			if (number % 15==0)
+				return "FizzBuzz";
+			if (number % 3 == 0)
+				return "Fizz";
+			if (number % 5==0)
+				return "Buzz";
+			return String.valueOf(number);
+		}
+		private static boolean isPrime(int number){
+			if ((number>0) && (number % 1 == 0) && (number % number == 0)){
+				for (int i = 2; i <number ; i++)
+				{
+					if ((number % i == 0) == true)
+						return false;
+				}
+			}
+			if (number != 1)
+				return true;
+			else
+				return false;
+
+		}
 }
--- 26-tdd/src/test/java/FizzBuzzTest.java
+++ 26-tdd/src/test/java/FizzBuzzTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import pl.sdacademy.tdd.FizzBuzz;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Fail.fail;
@@ -7,13 +10,29 @@ import static org.assertj.core.api.Fail.fail;
 // TODO: write your tests inside this class
 class FizzBuzzTest {
 
-	// this is how a test look like
-	@DisplayName("do nothing - this pseudo test is here only to show how to create your own tests")
+	@DisplayName("should return any non-empty string when number is 0")
 	@Test
-	void nothing() {
-		// this is how you write assertions
-		assertThat(1).isEqualTo(1);
-		assertThat(1).isNotEqualTo(2);
-		fail("remove this pseudo test and start writing your own real tests");
+	void anyString() throws Exception{
+		//when
+		String result = FizzBuzz.msg(0);
+		//then
+		assertThat(result).isNotEmpty();
+	}
+
+	/*@DisplayName("should return \"1\" when number is 1")
+	@Test
+	void one() {
+		//when
+		String msg = FizzBuzz.msg(1);
+		//then
+		assertThat(msg).isEqualTo("1");
+	}*/
+
+	@DisplayName("should return string according FizzBuzz specyfication")
+	@ParameterizedTest(name = "given = {0}, expected = \"{1}\"")
+	@CsvSource({"1, 1","2, Whiz","3, FizzWhiz","4, 4","5, BuzzWhiz","6, Fizz","7, Whiz","10, Buzz","11, Whiz",
+		"13, Whiz","17, Whiz","19, Whiz","15, FizzBuzz","30, FizzBuzz","97, Whiz"})
+	void testFizzBuzz(int number, String expectedMsg) throws Exception{
+		assertThat(FizzBuzz.msg(number)).isEqualTo(expectedMsg);
 	}
 }
